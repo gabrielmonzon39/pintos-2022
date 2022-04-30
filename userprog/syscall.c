040aa06e4a2dca394338881211aa47a3994dfeda
BrandonD0 <19005926@galileo.edu>
2022-04-30 01:59:41 -0600

    [Syscall-Handler] Agregando el mapeo de las syscalls

--- userprog/syscall.c
+++ userprog/syscall.c
@@ -12,9 +12,51 @@ syscall_init (void)
   intr_register_int (0x30, 3, INTR_ON, syscall_handler, "syscall");
 }
 
+static void check_num(int num, struct intr_frame *f);
+int not_valid(int num); 
+
 static void
 syscall_handler (struct intr_frame *f UNUSED) 
 {
-  printf ("system call!\n");
-  thread_exit ();
+  int num;
+  memread_user(f->esp, &num, sizeof(num));
+  check_num(num, f);
+}
+
+/* 0  - Halt
+ * 1  - Exit
+ * 2  - Execute
+ * 3  - Wait 
+ * 4  - Create
+ * 5  - Remove
+ * 6  - Open
+ * 7  - Filesize
+ * 8  - Read
+ * 9  - Write
+ * 10 - Seek
+ * 11 - Tell
+ * 12 - Close
+ * .. - Invalida
+*/
+
+int not_valid(int num) {
+  if (num >= 0 && num <= 12) return 0;
+  return 1;
+}
+
+static void check_num(int num, struct intr_frame *f) {
+  if (num ==  0)     do_halt(num, f);
+  if (num ==  1)     do_exit(num, f);
+  if (num ==  2)  do_execute(num, f);
+  if (num ==  3)     do_wait(num, f);
+  if (num ==  4)   do_create(num, f);
+  if (num ==  5)   do_remove(num, f);
+  if (num ==  6)     do_open(num, f);
+  if (num ==  7) do_filesize(num, f);
+  if (num ==  8)     do_read(num, f);
+  if (num ==  9)    do_write(num, f);
+  if (num == 10)     do_seek(num, f);
+  if (num == 11)     do_tell(num, f);
+  if (num == 12)    do_close(num, f);
+  if (not_valid(num)) do_not_valid(num, f);
 }
